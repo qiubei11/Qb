80a3d0164d5a1c4ff99bc99161b7af837bd6f359
Your Name <you@example.com>
2018-07-16 11:56:02 +0800

    program

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style >
+#app {
+  height: 700px;
+  display:flex;
+  display: -webkit-flex;
+  align-items:center;
+  justify-content:center; 
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+   <h1>武汉理工大学信息管理与信息系统</h1>
+   <h1>{{msg}}</h1>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '李秋贝信管1601'    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+
+h1 {
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  font-size:24px;
+  padding:1rem;
+  width:28rem; 
+}
+</style>
